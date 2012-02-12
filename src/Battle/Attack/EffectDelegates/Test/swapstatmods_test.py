a204b9835d9b003384fa54dc35b6948789fd4c8d
Chris Loew <cloew@mymail.mines.edu>
2012-02-12 14:39:57 -0700

    Added a test to Swap Stat Mods Effect

--- src/Battle/Attack/EffectDelegates/Test/swapstatmods_test.py
+++ src/Battle/Attack/EffectDelegates/Test/swapstatmods_test.py
@@ -1,45 +1,39 @@
-from Battle.Attack.EffectDelegates.swapstatmods_delegate import SwapStatModsDelegate
-
-from Battle.battle_side import BattleSide
-from Battle.pkmn_battle_wrapper import PkmnBattleWrapper
-from Pokemon.pokemon import Pokemon
-from Trainer.trainer import Trainer
-
 import unittest
+from Test.test_helper import BuildPokemonBattleWrapper
+
+from Battle.Attack.EffectDelegates.swapstatmods_delegate import SwapStatModsDelegate
 
 class applyEffect(unittest.TestCase):
     """ Test that isCharging returns the correct values """
     def setUp(self):
         """ Grabs the message dictionary from StatModDelegate """
-        trainer = Trainer()
-        pokemon = Pokemon("BULBASAUR")
-        trainer.beltPokemon = [pokemon]
-        
-        side1 = BattleSide(trainer)
-        self.wrapper1 = PkmnBattleWrapper(side1)
-        self.wrapper1.pkmn = pokemon
-        
-        side2 = BattleSide(trainer)
-        self.wrapper2 = PkmnBattleWrapper(side2)
-        self.wrapper2.pkmn = pokemon
+        self.user = BuildPokemonBattleWrapper()
+        self.target = BuildPokemonBattleWrapper()
         
         self.delegate = SwapStatModsDelegate()
         
         self.statMods1 = {"ATK":1, "DEF":0, "SPD":0, "SATK":0, "SDEF":0, 
                                 "ACC":0, "EVAS":0, "CRT":1}
-        self.wrapper1.statMods = self.statMods1
+        self.user.statMods = self.statMods1
                                 
         self.statMods2 = {"ATK":0, "DEF":0, "SPD":1, "SATK":1, "SDEF":1, 
                                 "ACC":0, "EVAS":0, "CRT":0}
-        self.wrapper2.statMods = self.statMods2
+        self.target.statMods = self.statMods2
         
     def statModsSwitched(self):
         """ Tests if ithe stat mods are swapped """
-        self.delegate.applyEffect(self.wrapper1, self.wrapper2)
-        assert self.wrapper1.statMods == self.statMods2, "Pkmn 1 should have stats from Pkmn 2"
-        assert self.wrapper2.statMods == self.statMods1, "Pkmn 2 should have stats from Pkmn 1"
+        self.delegate.applyEffect(self.user, self.target)
+        assert self.user.statMods == self.statMods2, "Pkmn 1 should have stats from Pkmn 2"
+        assert self.target.statMods == self.statMods1, "Pkmn 2 should have stats from Pkmn 1"
         
-testcasesApplyEffect= ["statModsSwitched"]
+    def message(self):
+        """ Test the message returned is correct """
+        messages = self.delegate.applyEffect(self.user, self.target)
+        message = self.user.getHeader() + SwapStatModsDelegate.message
+        assert len(messages) == 1, "Should get 1 message"
+        assert messages[0] == message, "Should be Pkmn's header and the Delegate's message"
+        
+testcasesApplyEffect= ["statModsSwitched", "message"]
 suiteApplyEffect = unittest.TestSuite(map(applyEffect, testcasesApplyEffect))
 
 ##########################################################
--- src/Battle/Attack/EffectDelegates/swapstatmods_delegate.py
+++ src/Battle/Attack/EffectDelegates/swapstatmods_delegate.py
@@ -1,10 +1,11 @@
 
 class SwapStatModsDelegate:
     """ Effect that swaps stat boosts between sides """
+    message = " switched stat changes with the target!"
     
     def applyEffect(self, user, target):
         """ Applies the delegate's effect when the attack hits """
         temp = target.statMods
         target.statMods = user.statMods
         user.statMods = temp
-        return [user.pkmn.name + " switched stat changes with the target!"]
+        return [user.getHeader() + SwapStatModsDelegate.message]
