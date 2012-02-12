b4033e9ee69dcd40760a442ad2e042eaf0ccadc0
Chris Loew <cloew@mymail.mines.edu>
2012-02-12 14:44:31 -0700

    Cleaned Trap Delegate Test

--- src/Battle/Attack/EffectDelegates/Test/trapdelegate_test.py
+++ src/Battle/Attack/EffectDelegates/Test/trapdelegate_test.py
@@ -1,33 +1,23 @@
-from Battle.Attack.EffectDelegates.trap_delegate import TrapDelegate
-
-from Battle.Attack.attack import Attack
-from Battle.SecondaryEffects.trap import Trap
-from Battle.battle_side import BattleSide
-from Battle.pkmn_battle_wrapper import PkmnBattleWrapper
-from Pokemon.pokemon import Pokemon
-from Trainer.trainer import Trainer
-
 import unittest
+from Test.test_helper import BuildPokemonBattleWrapper
+
+from Battle.Attack.EffectDelegates.trap_delegate import TrapDelegate
+from Battle.SecondaryEffects.trap import Trap
 
 class applyEffect(unittest.TestCase):
     """ Test that applyEffect actually adds a trap effect """
     
     def setUp(self):
         """ Builds the delegate and side for use in the tests """
-        trainer = Trainer()
-        pokemon = Pokemon("BULBASAUR")
-        trainer.beltPokemon = [pokemon]
-        side = BattleSide(trainer)
-        self.wrapper = PkmnBattleWrapper(side)
-        self.wrapper.pkmn = pokemon
+        self.pkmn = BuildPokemonBattleWrapper()
         self.delegate = TrapDelegate("", "", "")
         
     def appliesTrap(self):
         """ Tests if applyEffect applies the trap """
-        self.wrapper.secondaryEffects = []
-        self.delegate.applyEffect(None, self.wrapper)
+        self.pkmn.secondaryEffects = []
+        self.delegate.applyEffect(None, self.pkmn)
         
-        assert isinstance(self.wrapper.secondaryEffects[0], Trap), "Should have a trap effect"
+        assert isinstance(self.pkmn.secondaryEffects[0], Trap), "Should have a trap effect"
         
 testcasesApplyEffect = ["appliesTrap"]
 suiteApplyEffect = unittest.TestSuite(map(applyEffect, testcasesApplyEffect))
@@ -39,21 +29,16 @@ class removePreviousTrap(unittest.TestCase):
     
     def setUp(self):
         """ Builds the delegate and side for use in the tests """
-        trainer = Trainer()
-        pokemon = Pokemon("BULBASAUR")
-        trainer.beltPokemon = [pokemon]
-        side = BattleSide(trainer)
-        self.wrapper = PkmnBattleWrapper(side)
-        self.wrapper.pkmn = pokemon
+        self.pkmn = BuildPokemonBattleWrapper()
         self.delegate = TrapDelegate("", "", "")
         self.trap = Trap("", "")
         
     def removeTrap(self):
         """ Tests if the trap is removed """
-        self.wrapper.secondaryEffects = [self.trap]
-        self.delegate.removePreviousTrap(self.wrapper)
+        self.pkmn.secondaryEffects = [self.trap]
+        self.delegate.removePreviousTrap(self.pkmn)
         
-        assert not self.trap in self.wrapper.secondaryEffects, "Should have removed original trap effect"
+        assert not self.trap in self.pkmn.secondaryEffects, "Should have removed original trap effect"
         
 testcasesRemovePreviousTrap = ["removeTrap"]
 suiteRemovePreviousTrap = unittest.TestSuite(map(removePreviousTrap, testcasesRemovePreviousTrap))
@@ -65,33 +50,28 @@ class hasThisTrap(unittest.TestCase):
     
     def setUp(self):
         """ Builds the delegate and side for use in the tests """
-        trainer = Trainer()
-        pokemon = Pokemon("BULBASAUR")
-        trainer.beltPokemon = [pokemon]
-        side = BattleSide(trainer)
-        self.wrapper = PkmnBattleWrapper(side)
-        self.wrapper.pkmn = pokemon
+        self.pkmn = BuildPokemonBattleWrapper()
         self.delegate = TrapDelegate("", "", "")
         self.trap = Trap("", "")
         self.otherTrap = Trap("other.", "")
         
     def hasTrap(self):
         """ Tests if hasThisTrap returns true when there is an object of this trap """
-        self.wrapper.secondaryEffects = [self.trap]
+        self.pkmn.secondaryEffects = [self.trap]
         
-        assert self.delegate.hasThisTrap(self.wrapper), "Should have a trap effect"
+        assert self.delegate.hasThisTrap(self.pkmn), "Should have a trap effect"
         
     def noTrap(self):
         """ Tests if hasThisTrap returns false when there is no trap """
-        self.wrapper.secondaryEffects = []
+        self.pkmn.secondaryEffects = []
         
-        assert not self.delegate.hasThisTrap(self.wrapper), "Should not have a trap effect"
+        assert not self.delegate.hasThisTrap(self.pkmn), "Should not have a trap effect"
         
     def otherTrap(self):
         """ Tests if hasThisTrap returns false when there is a different trap """
-        self.wrapper.secondaryEffects = [self.otherTrap]
+        self.pkmn.secondaryEffects = [self.otherTrap]
         
-        assert not self.delegate.hasThisTrap(self.wrapper), "Should not have a trap effect if there is a different trap effect there"
+        assert not self.delegate.hasThisTrap(self.pkmn), "Should not have a trap effect if there is a different trap effect there"
         
 testcasesHasThisTrap = ["hasTrap", "noTrap", "otherTrap"]
 suiteHasThisTrap = unittest.TestSuite(map(hasThisTrap, testcasesHasThisTrap))
