d0adbc7513b764cc9d1459766a9f52883fca372f
Chris Loew <cloew@mymail.mines.edu>
2012-02-14 18:28:09 -0700

    Updated to use Pkmn Battle Wrapper

--- src/Battle/SecondaryEffects/Test/periodicheal_test.py
+++ src/Battle/SecondaryEffects/Test/periodicheal_test.py
@@ -1,30 +1,26 @@
+import unittest
+from Test.test_helper import BuildPokemonBattleWrapper
+
 from Battle.SecondaryEffects.periodic_heal import PeriodicHeal
 from Battle.battle_side import BattleSide
 from Battle.pkmn_battle_wrapper import PkmnBattleWrapper
 from Pokemon.pokemon import Pokemon
 from Trainer.trainer import Trainer
 
-import unittest
-
 class afterTurn(unittest.TestCase):
     """ Test that afterTurn returns correctly """
     
     def setUp(self):
-        """ Builds the side and heal """
-        trainer = Trainer()
-        pokemon = Pokemon("BULBASAUR")
-        trainer.beltPokemon = [pokemon]
-        side = BattleSide(trainer)
-        self.wrapper = PkmnBattleWrapper(side)
-        self.wrapper.pkmn = pokemon
+        """ Build the Pkmn and Heal """
+        self.pkmn = BuildPokemonBattleWrapper()
         
         self.message = " hurt."
         self.heal = PeriodicHeal(self.message)
         
     def message(self):
         """ Test the message is correct """
-        message = self.heal.afterTurn(self.wrapper)
-        assert message == [self.wrapper.getHeader() + self.message], "Message should be the pokemon's name and the message given to the Heal."
+        message = self.heal.afterTurn(self.pkmn)
+        assert message == [self.pkmn.getHeader() + self.message], "Message should be the pokemon's name and the message given to the Heal."
         
 testcasesAfterTurn = ["message"]
 suiteAfterTurn = unittest.TestSuite(map(afterTurn, testcasesAfterTurn))
@@ -36,14 +32,14 @@ class getHeal(unittest.TestCase):
     
     def setUp(self):
         """ Builds the heal """
-        self.pokemon = Pokemon("BULBASAUR")
+        self.pkmn = BuildPokemonBattleWrapper()
         self.heal = PeriodicHeal("")
-        self.hp = 32.0
+        self.hp = 32
     
     def heal(self):
         """ Test the heal returns the proper ratio """
-        self.pokemon.battleDelegate.stats["HP"] = self.hp
-        heal = self.heal.getHeal(self.pokemon)
+        self.pkmn.setStat("HP", self.hp)
+        heal = self.heal.getHeal(self.pkmn)
         assert heal == self.hp/PeriodicHeal.ratio, "Heal should be a sixteenth of the targets health"
         
 testcasesGetHeal = ["heal"]
