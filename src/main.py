313970a7eb27ae947fbf9905aa8645f6193219d5
Chris Loew <cloew@mymail.mines.edu>
2012-03-11 21:36:02 -0600

    Tweaked trainerdex.xml and removed earlier trainer files from main.py

--- src/main.py
+++ src/main.py
@@ -4,16 +4,10 @@ from Trainer.human_trainer import HumanTrainer
 
 from Trainer.trainer_factory import TrainerFactory
 
-file = open("resources/trainer-chris", 'r')
-file2 = open("resources/trainer-eric", 'r')
-
 trainer = HumanTrainer()
 TrainerFactory.loadFromXML(trainer, "Chris")
 trainer2 = ComputerTrainer()
 TrainerFactory.loadFromXML(trainer2, "Eric")
-
-file.close()
-file2.close()
 
 BattleViewController().begin(trainer, trainer2)
 
