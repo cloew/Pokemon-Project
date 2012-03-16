b03d91c639b5e6ca39fa071500acacb91d5634a6
Chris Loew <cloew@mymail.mines.edu>
2012-03-16 17:11:12 -0600

    Removed Side And action since it has been replaced by the new and improved Battle Actions

--- src/Battle/sideandaction.py
+++
@@ -1,8 +0,0 @@
-
-class SideAndAction:
-    """ Holds a side and its corresponding action """
-    
-    def __init__(self, side, action):
-        """ Build a SideAndAction from a side and action """
-        self.side = side
-        self.action = action
