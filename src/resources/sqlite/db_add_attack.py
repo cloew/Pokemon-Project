3dfd06fa6b4c45250c0733bedfa060f3aabc020e
Chris Loew <cloew@mymail.mines.edu>
2012-04-10 18:21:24 -0600

    Add ATtack now checks if the attack has already been added

--- src/resources/sqlite/db_add_attack.py
+++ src/resources/sqlite/db_add_attack.py
@@ -1,4 +1,5 @@
 import sqlite3
+from resources.sqlite.pokemon_sqlite_helper import PkmnDBConnect
 
 class DBAddAttack:
     """ Adds an attack to the Database """
@@ -7,18 +8,27 @@ class DBAddAttack:
         self.commands = {'p':self.addParameters, 'h':self.addHit,
                                    'd':self.addDamage, 'c':self.addCrit, 's':self.addSpeed}
         self.vals = {'name':None, 'type':None}
+        
+        
+        self.connection = PkmnDBConnect()
+        self.cursor = self.connection.cursor()
     
     def execute(self, params):
         """  """
         for param in params:
             cmd = param[0]
-            cmdParams = param[2:]
+            cmdParams = param[2:].split(':')
             
             self.commands[cmd](cmdParams)
             
+        self.connection.close()
+            
     def addParameters(self, vals):
-        """  """
-        print vals
+        """ Adds Attack specific parameters """
+        self.vals['name'] = vals[0]
+        self.vals['type'] = vals[1]
+        
+        self.checkForAttackAlready()
         
     def addHit(self, vals):
         """  """
@@ -35,3 +45,12 @@ class DBAddAttack:
     def addSpeed(self, vals):
         """  """
         print vals
+        
+    def checkForAttackAlready(self):
+        """  """
+        self.cursor.execute("SELECT id from Attack where name = ?", (self.vals['name'],))
+        t = self.cursor.fetchone()
+        
+        if not t is None:
+            print "Attack %s already exists" % self.vals['name']
+            exit(-2)
