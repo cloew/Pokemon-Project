6fba22ee4014891da9244409b3051c454f170ff6
Chris Loew <cloew@mymail.mines.edu>
2012-05-20 10:00:32 -0600

    Added MenuEntry class to represent the entries in the main menu

--- src/Screen/GUI/MainMenu/menu.py
+++ src/Screen/GUI/MainMenu/menu.py
@@ -1,18 +1,18 @@
 import pygame
 
+from menu_entry import MenuEntry
+
 class Menu:
     """ Represents the menu on the main menu screen """
     
     def __init__(self):
         """ Build the menu """
-        self.font = pygame.font.SysFont("Times New Roman", 36)
+        self.entry = MenuEntry("Start Game")
         
     def draw(self, window): 
         """ Draw the menu """
-        text = self.font.render("Start Game", 1, (10, 10, 10))
-        textpos = text.get_rect(centerx = window.get_width()/2, centery= window.get_height()/2)
-        window.blit(text, textpos)
+        self.entry.draw(window)
         
     def setBold(self, bold):
         """ Set the boldness of the font """
-        self.font.set_bold(bold)
+        self.entry.setBold(bold)
---
+++ src/Screen/GUI/MainMenu/menu_entry.py
@@ -0,0 +1,19 @@
+import pygame
+
+class MenuEntry:
+    """ Represents an entry in the menu """
+    
+    def __init__(self, text):
+        """ Sets the entry's text """
+        self.font = pygame.font.SysFont("Times New Roman", 36)
+        self.text = text
+        
+    def draw(self, window):
+        """ Draws the menu entry on the window """
+        text = self.font.render(self.text, 1, (10, 10, 10))
+        textpos = text.get_rect(centerx = window.get_width()/2, centery= window.get_height()/2)
+        window.blit(text, textpos)
+        
+    def setBold(self, bold):
+        """ Sets the Boldness of the entry """
+        self.font.set_bold(bold)
