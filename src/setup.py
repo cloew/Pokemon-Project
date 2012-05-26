d48e7b834d18ada042b641d8e8fd3d680de1293a
Chris Loew <cloew@mymail.mines.edu>
2012-05-26 16:54:07 -0600

    Updated setup.py so it can add the font module

--- src/setup.py
+++ src/setup.py
@@ -1,4 +1,13 @@
 from distutils.core import setup
 import py2exe
+import os
+
+origIsSystemDLL = py2exe.build_exe.isSystemDLL # save the orginal before we edit it
+def isSystemDLL(pathname):
+    # checks if the freetype and ogg dll files are being included
+    if os.path.basename(pathname).lower() in ("libfreetype-6.dll", "libogg-0.dll", "sdl_ttf.dll"):
+            return 0
+    return origIsSystemDLL(pathname) # return the orginal function
+py2exe.build_exe.isSystemDLL = isSystemDLL # override the default function with this one
 
 setup(console=['MainMenu/main_menu_controller.py'])
