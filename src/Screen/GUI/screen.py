f2bf12365f2171bc0ce9ec6d5fac37190c94eba5
Chris Loew <cloew@mymail.mines.edu>
2012-07-12 21:39:52 -0600

    Adding stubbed out screen and view

--- src/Screen/GUI/screen.py
+++ src/Screen/GUI/screen.py
@@ -1,50 +1,12 @@
-import sys
-
-import pygame
-from pygame.locals import *
-from Screen.GUI.pygame_helper import load_image
 
 class Screen:
-    """ Represents the screen """
-    GAME_SPEED = 60
+    """ Represents a screen displayed in the game """
     
     def __init__(self):
-        """ Build the window """
-        self.window = self.getWindow()
-        self.clock = pygame.time.Clock()
-        
-    def setScreen(self, screen):
-        """ Sets the current Screen Display """
-        self.screen = screen
-        
-    def getWindow(self):
-        """ Gets the GUI Window """
-        pygame.init()
-        pygame.display.set_icon(self.getIcon())
-        pygame.display.set_caption('Pokemon')
-        pygame.mouse.set_visible(0)
-        return pygame.display.set_mode((640, 480))
-        
-    def getIcon(self):
-        """ Gets the icon """
-        image = load_image("pokeball.png")
-        return image
+        """ Build the Screen """
         
     def update(self):
-        """ Update the screen """
-        self.clock.tick(self.GAME_SPEED)
-        self.screen.update()
+        """ Update the screen. Should be overridden in sub-class """
         
-    def draw(self):
-        """ Draws the screen """
-        self.screen.draw(self.window)
-        self.redraw()
-            
-    def redraw(self):
-        pygame.display.flip()
-        
-    def clearWindow(self):
-        """ Clears the window """
-        self.window.fill((0,0,0))
-        
-screen = Screen()
+    def draw(self, window):
+        """ Draws the screen to the Window provided. Should be overridden in sub-class """ 
