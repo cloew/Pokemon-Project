3bb44ae6a57f3895d98a6229cc50e958a3651117
Chris Loew <cloew@mymail.mines.edu>
2012-09-23 12:07:02 -0600

    Added Action Menu class

---
+++ src/Menu/ActionMenu/action_menu.py
@@ -0,0 +1,22 @@
+
+class ActionMenu:
+    """ Represents the Battle's Action Menu """
+    
+    def __init__(self):
+        """  """
+        self.entries = []
+        
+    def select(self):
+        """ Selects the current menu entry """
+        
+    def up(self):
+        """ Selects the entry one up from the one highlighted """
+        
+    def down(self):
+        """ Selects the entry one down from the one highlighted """
+        
+    def right(self):
+        """ Selects the entry one right from the one highlighted """
+        
+    def left(self):
+        """ Selects the entry one left from the one highlighted """
