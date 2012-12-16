76c86beeff9c8a7a754ba8100262e60ddd1bf958
Chris Loew <cloew123@gmail.com>
2012-12-16 11:27:52 -0700

    Added some magic logic to make the Pokemon Menu Entry more than one line

--- src/Screen/Console/Menu/PokemonMenu/pokemon_menu_entry_view.py
+++ src/Screen/Console/Menu/PokemonMenu/pokemon_menu_entry_view.py
@@ -8,7 +8,50 @@ class PokemonMenuEntryView(MenuEntryView):
         format = self.getTerminalFormatting(self.entry.selected, window.terminal)
         pkmn = self.entry.getPokemon()
         return "{0}{1}--{2}{t.normal}".format(format, pkmn.name, pkmn.species, t=window.terminal)
+
+    def getEntrySize(self, window):
+        """ Return the entry size """
+        return (window.width/2 - 2, window.height/3 - 2)
+
+    def getEntryLines(self, window):
+        """ Return entry lines """
+        lines = []
+        entrySize = self.getEntrySize(window)
+
+        lines.append(self.getHeaderLine())
+        lines.append(self.getPokemonNameLine())
+        lines.append(self.getHealthLine())
+        for i in range(entrySize[1]-4):
+            lines.append("")
+        lines.append(self.getHeaderLine())
+        return lines
         
+    def getHeaderLine(self, entrySize):
+        """ Returns the line for the header and footer of the entry """
+        return "|{0}|".format("-"*(entrySize[0]-2))
+
+    def getLine(self, text, window):
+        """ Returns a line """
+        entrySize = self.getEntrySize(window)
+        spacer = " "*(entrySize[0]-2-len(text))
+        format = self.getTerminalFormatting(self.entry.selected, window.terminal)
+        return "|{0}{1}{2}{t.normal}|".format(format, text, spacer t=window.terminal)
+
+    def getPokemonNameLine(self):
+        """ Returns the Pokemon Name and Species as a string """
+        pkmn = self.entry.getPokemon()
+        nameString = "{0} --- {1}".format(self.getFullString(pkmn.name), self.getFullString(pkmn.species))
+        return self.getLine(nameString)
+
+    def getHealthLine(self):
+        """ Return the line with health stats """
+        pkmn = self.entry.getPokemon()
+        return self.getLine("{0}/{1}".format(pkmn.getCurrHP(), pkmn.getStat("HP")))
+
+    def getFullString(self, string):
+        """ Returns a string with the full 10 char limit """
+        return (string + " "*10)[:10]
+
     def getTerminalFormatting(self, selected, terminal):
         """ Sets the Boldness of the entry """
         if selected:
