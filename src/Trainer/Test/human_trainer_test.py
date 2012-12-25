4d4c78064a6ae9422f11c1ee5cbbc3cc14d0605f
Chris Loew <cloew123@gmail.com>
2012-12-25 10:42:04 -0700

    Fixed Trainer tests to use the new Pick Action and Get Action functions

--- src/Trainer/Test/human_trainer_test.py
+++ src/Trainer/Test/human_trainer_test.py
@@ -3,24 +3,7 @@ import unittest
 from Trainer.human_trainer import HumanTrainer
 
 class getHeader(unittest.TestCase):
-    """ Test cases of getFirstPokemon """
-    
-    def  setUp(self):
-        """ Build the Trainer and Pokemon lists for use in tests """
-        self.trainer = HumanTrainer()
-        
-    def headerIsHumanHeader(self):
-        """ Check that the attack returned by getAction is an attack the Pokemon has """
-        assert self.trainer.getHeader() is HumanTrainer.header, "Should be an empty string"
-
-# Collect all test cases in this class
-testcasesGetHeader = ["headerIsHumanHeader"]
-suiteGetHeader   = unittest.TestSuite(map(getHeader , testcasesGetHeader ))
-
-##########################################################
-
-class getHeader(unittest.TestCase):
-    """ Test cases of getFirstPokemon """
+    """ Test cases of getHeader """
     
     def  setUp(self):
         """ Build the Trainer and Pokemon lists for use in tests """
