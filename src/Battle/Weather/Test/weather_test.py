b147edf8ac8499e679234f7c5539c1fb762fce9f
Chris Loew <cloew123@gmail.com>
2012-12-27 19:05:12 -0700

    Added test of the weather effect on a particular pokemon

--- src/Battle/Weather/Test/weather_test.py
+++ src/Battle/Weather/Test/weather_test.py
@@ -7,7 +7,7 @@ class betweenTurns(unittest.TestCase):
     """ Test cases of betweenTurns """
     
     def  setUp(self):
-        """ Build the *** for the test """
+        """ Build the BattleSides and weather for the test """
         self.message = "Some Message"
         self.weather = Weather(self.message)
         self.playerSide = BuildBattleSide()
@@ -24,8 +24,28 @@ suiteBetweenTurns = unittest.TestSuite(map(betweenTurns, testcasesBetweenTurns))
 
 ##########################################################
 
+class performWeatherEffectOnPokemon(unittest.TestCase):
+    """ Test cases of performWeatherEffectOnPokemon """
+    
+    def  setUp(self):
+        """ Build the Pokemon Battle Wrapper and weather for the test """
+        self.message = "Some Message"
+        self.weather = Weather(self.message)
+        self.pkmn = BuildPokemonBattleWrapper()
+        
+    def performWeatherEffectOnPokemonMessage(self):
+        """ Test that performWeatherEffectOnPokemon returns the Weather's between turns message """
+        messages = self.weather.performWeatherEffectOnPokemon(self.pkmn)
+        assert messages == [], "Should receive an empty between turn message for each Pokemon"
+
+# Collect all test cases in this class
+testcasesPerformWeatherEffectOnPokemon = ["performWeatherEffectOnPokemonMessage"]
+suitePerformWeatherEffectOnPokemon = unittest.TestSuite(map(performWeatherEffectOnPokemon, testcasesPerformWeatherEffectOnPokemon))
+
+##########################################################
+
 # Collect all test cases in this file
-suites = [suiteBetweenTurns]
+suites = [suiteBetweenTurns, suitePerformWeatherEffectOnPokemon]
 suite = unittest.TestSuite(suites)
 
 if __name__ == "__main__":
