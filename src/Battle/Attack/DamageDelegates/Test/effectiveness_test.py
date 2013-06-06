74de81de59d3ba36115d7df93512c5c7bc3ec297
Chris Loew <cloew123@gmail.com>
2013-06-06 17:30:59 -0600

    Updated Effectiveness Table

--- src/Battle/Attack/DamageDelegates/Test/effectiveness_test.py
+++ src/Battle/Attack/DamageDelegates/Test/effectiveness_test.py
@@ -5,28 +5,32 @@ import unittest
 class getMod(unittest.TestCase):
     """ Test the getMod function """
     
+    def setUp(self):
+        """  """
+        self.effectivenessTable = Effectiveness()
+    
     def immune(self):
         """ Test that the correct values are returned when the 
         Pokemon is immune to the Attack Type """
-        mod = Effectiveness.getMod("GHOST", "NORMAL")
+        mod = self.effectivenessTable.getMod("GHOST", "NORMAL")
         assert mod is 0, "GHOST on NORMAL should be immune"
         
     def ineffective(self):
         """ Test that the correct values are returned when the 
         Pokemon is resistant to the Attack Type """
-        mod = Effectiveness.getMod("FIRE", "FIRE")
+        mod = self.effectivenessTable.getMod("FIRE", "FIRE")
         assert mod == .5, "FIRE on FIRE should be ineffective"
         
     def effective(self):
         """ Test that the correct values are returned when the 
         Pokemon is indifferent to the Attack Type """
-        mod = Effectiveness.getMod("NORMAL", "NORMAL")
+        mod = self.effectivenessTable.getMod("NORMAL", "NORMAL")
         assert mod is 1, "NORMAL on NORMAL should be effective"
         
     def supereffective(self):
         """ Test that the correct values are returned when the 
         Pokemon is weak to the Attack Type """
-        mod = Effectiveness.getMod("FIRE", "ICE")
+        mod = self.effectivenessTable.getMod("FIRE", "ICE")
         assert mod is 2, "FIRE on ICE should be super-effective"
        
 # Collect all test cases for this class       
@@ -38,37 +42,41 @@ suiteGetMod = unittest.TestSuite(map(getMod, testcasesGetMod))
 class respond(unittest.TestCase):
     """ Test the respondfunction """
     
+    def setUp(self):
+        """  """
+        self.effectivenessTable = Effectiveness()
+    
     def immune(self):
         """ Test that the correct values are returned when the 
         Pokemon is immune to the Attack Type """
-        message = Effectiveness.respond(0)
+        message = self.effectivenessTable.respond(0)
         assert message == Effectiveness.immuneResponse, \
                 "Response should be the immune response"
         
     def ineffective(self):
         """ Test that the correct values are returned when the 
         Pokemon is resistant to the Attack Type """
-        message = Effectiveness.respond(.25)
+        message = self.effectivenessTable.respond(.25)
         assert message == Effectiveness.ineffectiveResponse, \
                 "Response should be the ineffective response"
-        message = Effectiveness.respond(.5)
+        message = self.effectivenessTable.respond(.5)
         assert message == Effectiveness.ineffectiveResponse, \
                 "Response should be the ineffective response"
         
     def effective(self):
         """ Test that the correct values are returned when the 
         Pokemon is indifferent to the Attack Type """
-        message = Effectiveness.respond(1)
+        message = self.effectivenessTable.respond(1)
         assert message == Effectiveness.effectiveResponse, \
                 "Response should be the effective response"
         
     def supereffective(self):
         """ Test that the correct values are returned when the 
         Pokemon is weak to the Attack Type """
-        message = Effectiveness.respond(2)
+        message = self.effectivenessTable.respond(2)
         assert message == Effectiveness.superEffectiveResponse, \
                 "Response should be the super-effective response"
-        message = Effectiveness.respond(4)
+        message = self.effectivenessTable.respond(4)
         assert message == Effectiveness.superEffectiveResponse,  \
                 "Response should be the super-effective response"
        
@@ -87,26 +95,27 @@ class getEffectiveness(unittest.TestCase):
         self.typeEffective = ["NORMAL"]
         self.typeSuperEffective = ["ICE"]
         self.typeSuperEffective2 = ["ICE", "GRASS"]
+        self.effectivenessTable = Effectiveness()
     
     def immune(self):
         """ Test that the correct values are returned when the 
         Pokemon is immune to the Attack Type """
-        mod, message = Effectiveness.getEffectiveness("GHOST", self.typeImmune)
+        mod, message = self.effectivenessTable.getEffectiveness("GHOST", self.typeImmune)
         assert mod is 0, "GHOST on NORMAL should be immune"
         
     def ineffective(self):
         """ Test that the correct values are returned when the 
         Pokemon is resistant to the Attack Type """
-        mod, message = Effectiveness.getEffectiveness("FIRE", self.typeIneffective)
+        mod, message = self.effectivenessTable.getEffectiveness("FIRE", self.typeIneffective)
         assert mod == .5, "FIRE on FIRE should be ineffective"
         
-        mod, message = Effectiveness.getEffectiveness("FIRE", self.typeIneffective2)
+        mod, message = self.effectivenessTable.getEffectiveness("FIRE", self.typeIneffective2)
         assert mod == .25, "FIRE on FIRE/ROCK should be 2x ineffective"
         
     def effective(self):
         """ Test that the correct values are returned when the 
         Pokemon is indifferent to the Attack Type """
-        mod, message = Effectiveness.getEffectiveness("NORMAL", self.typeEffective)
+        mod, message = self.effectivenessTable.getEffectiveness("NORMAL", self.typeEffective)
         assert mod is 1, "NORMAL on NORMAL should be effective"
         assert message is None,  \
                 "No message should be returned when the attack is unmodified"
@@ -114,10 +123,10 @@ class getEffectiveness(unittest.TestCase):
     def supereffective(self):
         """ Test that the correct values are returned when the 
         Pokemon is weak to the Attack Type """
-        mod, message = Effectiveness.getEffectiveness("FIRE", self.typeSuperEffective)
+        mod, message = self.effectivenessTable.getEffectiveness("FIRE", self.typeSuperEffective)
         assert mod is 2, "FIRE on ICE should be super-effective"
         
-        mod, message = Effectiveness.getEffectiveness("FIRE", self.typeSuperEffective2)
+        mod, message = self.effectivenessTable.getEffectiveness("FIRE", self.typeSuperEffective2)
         assert mod is 4, "FIRE on ICE/GRASS should be 2x super-effective"
         
 # Collect all test cases in this class      
