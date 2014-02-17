cb6ec49ba71b0c0398f95ad233e6763d42905421
Chris Loew <cloew123@gmail.com>
2014-02-17 14:21:01 -0700

    Some tidying up of the Pokemon Stats View so it now uses Labels for text that just needs to be displayed

--- src/Screen/Pygame/Battle/Side/pokemon_stats_view.py
+++ src/Screen/Pygame/Battle/Side/pokemon_stats_view.py
@@ -6,6 +6,7 @@ from Screen.Pygame.HealthBar.health_bar_view import HealthBarView
 from Screen.Pygame.Menu.MainMenu.menu_entry_view import MenuEntryView
 
 from kao_gui.pygame.pygame_widget import PygameWidget
+from kao_gui.pygame.widgets.label import Label
 
 class PokemonStatsView(PygameWidget):
     """ View for a Pokemon's Stats in a Battle """
@@ -20,6 +21,8 @@ class PokemonStatsView(PygameWidget):
             
         self.showHP = showHP
         self.setPokemonMenuEntryView(pokemonMenuEntry)
+        self.setLevelLabel()
+        self.setHealthLabel()
         self.healthBarView = HealthBarView(self.pokemon)
         
     def setSize(self, width, height):
@@ -38,36 +41,33 @@ class PokemonStatsView(PygameWidget):
         if pokemonMenuEntry is None:
             pokemonMenuEntry = PokemonMenuEntry(self.pokemon, None)
         self.pkmnEntryView = MenuEntryView(pokemonMenuEntry, self.FONT_SIZE)
-        self.setLevelMenuEntryView()
-        self.setHealthMenuEntryView()
         
-    def setLevelMenuEntryView(self):
-        """ Set the Level Menu Entry view """
-        menuEntry = TextMenuEntry("Lv. {0}".format(self.pokemon.getLevel()), None)
-        self.levelEntryView = MenuEntryView(menuEntry, self.FONT_SIZE)
+    def setLevelLabel(self):
+        """ Set the Level Label """
+        self.levelLabel = Label("Lv. {0}".format(self.pokemon.getLevel()), size=self.FONT_SIZE)
         
-    def setHealthMenuEntryView(self):
-        """ Set the Level Menu Entry view """
-        menuEntry = TextMenuEntry("{0}/{1}".format(self.pokemon.getCurrHP(), self.pokemon.getStat("HP")), None)
-        self.healthEntryView = MenuEntryView(menuEntry, self.FONT_SIZE)
+    def setHealthLabel(self):
+        """ Set the Health Label """
+        hpString = "{0}/{1}".format(self.pokemon.getCurrHP(), self.pokemon.getStat("HP"))
+        self.healthLabel = Label(hpString, size=self.FONT_SIZE)
         
     def drawSurface(self):
         """ Draw the Pokemon Stats View """
         pkmnSurface = self.pkmnEntryView.draw()
         self.drawOnSurface(pkmnSurface, left=0, top=0)
         
-        levelSurface = self.levelEntryView.draw()
+        levelSurface = self.levelLabel.draw()
         self.drawOnSurface(levelSurface, right=1, top=0)
         
         healthBarSurface = self.healthBarView.draw()
         self.drawOnSurface(healthBarSurface, left=0, top=(pkmnSurface.get_height()+10.0)/self.height)
         
         if self.showHP:
-            healthSurface = self.healthEntryView.draw()
+            healthSurface = self.healthLabel.draw()
             self.drawOnSurface(healthSurface, right=1, 
                     top=(pkmnSurface.get_height()+healthBarSurface.get_height()+15.0)/self.height)
         
     def update(self):
         """ Update the Pokemon Stats View """
-        self.setLevelMenuEntryView()
-        self.setHealthMenuEntryView()
+        self.setLevelLabel()
+        self.setHealthLabel()
