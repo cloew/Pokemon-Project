76b851aa56cb11407d37591c1ec56cc7b97af61c
Chris Loew <cloew123@gmail.com>
2014-02-17 13:42:28 -0700

    Options Menu Screen now uses KaoGUI Pygame Labels to render itself

--- src/Screen/Pygame/Menu/OptionsMenu/options_menu_screen.py
+++ src/Screen/Pygame/Menu/OptionsMenu/options_menu_screen.py
@@ -3,6 +3,7 @@ from Screen.Pygame.screen import Screen
 from Screen.Pygame.Menu.MainMenu.scrolling_map import map
 
 from kao_gui.pygame.pygame_screen import PygameScreen
+from kao_gui.pygame.widgets.label import Label
 
 import pygame
 
@@ -15,11 +16,11 @@ class OptionsMenuScreen(PygameScreen):
     def __init__(self, menu):
         """  """
         self.menu = menu
-        self.font = pygame.font.SysFont("Times New Roman", 36)
-        
-    def update(self):
-        """ Update the screen """
-        map.update()
+        self.commandLabels = []
+        self.keyLabels = []
+        for cmd in self.bindingsOrder:
+            self.commandLabels.append(Label(self.menu.cmdStrings[cmd], bold=True))
+            self.keyLabels.append(Label(self.menu.keyBindings[cmd]))
         
     def drawSurface(self):
         """ Draw the window """
@@ -35,23 +36,19 @@ class OptionsMenuScreen(PygameScreen):
         """ Draw Bindings Text """
         yRatio = 5.0
         
-        for cmd in self.bindingsOrder:
-            self.drawCommand(cmd, yRatio)
-            self.drawKeys(cmd, yRatio)
+        for i in range(len(self.bindingsOrder)):
+            self.drawCommand(i, yRatio)
+            self.drawKeys(i, yRatio)
             yRatio += 3
             
-    def drawCommand(self, cmd, yRatio):
+    def drawCommand(self, index, yRatio):
         """ Draws the command """
         cmdXRatio = 5.0/16
+        label = self.commandLabels[index]
+        self.drawOnSurface(label.draw(), right=cmdXRatio, centery=yRatio/32)
         
-        self.font.set_bold(True)
-        text = self.font.render(self.menu.cmdStrings[cmd], 1, (10, 10, 10))
-        self.drawOnSurface(text, right=cmdXRatio, centery=yRatio/32)
-        
-    def drawKeys(self, cmd, yRatio):
+    def drawKeys(self, index, yRatio):
         """ Draws the binding """
         bindingXRatio = 8.0/16
-        
-        self.font.set_bold(False)
-        text = self.font.render(self.menu.keyBindings[cmd], 1, (10, 10, 10))
-        self.drawOnSurface(text, left=bindingXRatio, centery=yRatio/32)
+        label = self.keyLabels[index]
+        self.drawOnSurface(label.draw(), left=bindingXRatio, centery=yRatio/32)
