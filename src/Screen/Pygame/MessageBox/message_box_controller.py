6a9b8b816e097bdb90fe56cce1bc90282193b90b
Chris Loew <cloew123@gmail.com>
2014-02-17 11:05:31 -0700

    Message Box Controller now uses the KaoGUI Pygame library

--- src/Screen/Pygame/MessageBox/message_box_controller.py
+++ src/Screen/Pygame/MessageBox/message_box_controller.py
@@ -1,21 +1,20 @@
 from InputProcessor import commands
-from Screen.Pygame.Controller.controller import Controller
 from Screen.Pygame.MessageBox.message_box_screen import MessageBoxScreen
 
-class MessageBoxController(Controller):
+from kao_gui.pygame.pygame_controller import PygameController
+
+class MessageBoxController(PygameController):
     """ Controller for a Message Box """
     
     def __init__(self, message, lastScreen):
         """ Initialize the Message Box Controller """
-        Controller.__init__(self)
         self.message = message
-        self.screen = MessageBoxScreen(message, lastScreen)
-        self.cmds = {commands.SELECT:self.closeMessageBox}
+        screen = MessageBoxScreen(message, lastScreen)
+        cmds = {commands.SELECT:self.closeMessageBox}
+        
+        PygameController.__init__(self, screen, commands=cmds)
         
     def closeMessageBox(self):
         """ Close the message box if it has finished showing """
         if self.message.fullyDisplayed:
             self.stopRunning()
-        
-    def update(self):
-        """ Do Nothing """
--- src/Screen/Pygame/MessageBox/message_box_screen.py
+++ src/Screen/Pygame/MessageBox/message_box_screen.py
@@ -1,8 +1,9 @@
-from Screen.Pygame.pygame_helper import GetTransparentSurface
 from Screen.Pygame.MessageBox.message_box import MessageBox
-from Screen.Pygame.screen import Screen
 
-class MessageBoxScreen(Screen):
+from kao_gui.pygame.pygame_helper import GetTransparentSurface
+from kao_gui.pygame.pygame_screen import PygameScreen
+
+class MessageBoxScreen(PygameScreen):
     """ Represents a message box screen """
     
     def __init__(self, message, lastScreen):
@@ -10,21 +11,20 @@ class MessageBoxScreen(Screen):
         self.messageBox = MessageBox(message)
         self.lastScreen = lastScreen
         
-    def draw(self, window):
+    def drawSurface(self, surface):
         """ Draw the Screen """
-        self.lastScreen.draw(window)
+        previousScreenSurface = self.lastScreen.draw()
+        self.drawOnSurface(previousScreenSurface, left=0, top=0)
         
-        width = window.width*.9
-        height = window.height*.3
+        width = surface.get_width()*.9
+        height = surface.get_height()*.3
         
-        surface = GetTransparentSurface(width, height)
+        messageBoxWindowSurface = GetTransparentSurface(width, height)
         messageBoxSurface = self.messageBox.draw()
+        
         messageBoxPosition = messageBoxSurface.get_rect(centerx=width*.5, centery=height*.5)
-        surface.blit(messageBoxSurface, messageBoxPosition)
-        
-        surfacePosition = surface.get_rect(left=window.width/20, top=window.height*.7)
-        window.draw(surface, surfacePosition)
-        
+        messageBoxWindowSurface.blit(messageBoxSurface, messageBoxPosition)
+        self.drawOnSurface(messageBoxWindowSurface, left=.05, top=.7)
         
     def update(self):
         """ Update the Screen """
