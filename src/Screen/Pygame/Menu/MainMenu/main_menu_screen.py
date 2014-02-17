792f247800af4e5609c2eac5956309d816483e60
Chris Loew <cloew123@gmail.com>
2014-02-16 22:10:29 -0700

    Main Menu Screen uses the KaoGUI Pygame Screen

--- src/Screen/Pygame/Menu/MainMenu/main_menu_screen.py
+++ src/Screen/Pygame/Menu/MainMenu/main_menu_screen.py
@@ -1,14 +1,10 @@
-import random
-
-import pygame
-from pygame.locals import *
-
-from Screen.Pygame.screen import Screen
 from scrolling_map import map
 from logo import Logo
 from menu_view import MenuView
 
-class MainMenuScreen(Screen):
+from kao_gui.pygame.pygame_screen import PygameScreen
+
+class MainMenuScreen(PygameScreen):
     """ Represents the Main Menu screen """
     
     def __init__(self, menu):
@@ -21,31 +17,26 @@ class MainMenuScreen(Screen):
         """ Update the screen """
         map.update()
         
-    def draw(self, window):
+    def drawSurface(self, surface):
         """ Draws the screen to the provided window """
-        surface = pygame.Surface((window.width, window.height))
         self.drawMap(surface)
         self.drawLogo(surface)
         self.drawMenu(surface)
-        return surface
         
     def drawMap(self, surface):
         """ Draws the map to the window """
         mapSurface = map.draw()
         surface.blit(mapSurface, (0, 0))
-        # window.draw(mapSurface, (0,0))
         
     def drawLogo(self, surface):
         """ Draws the Logo to the window """
         logoSurface = self.logo.draw()
-        logoPosition = self.getCenteredRect(surface, logoSurface, .5, .25) 
+        logoPosition = self.getCenteredPostition(logoSurface, .5, .25) 
         surface.blit(logoSurface, logoPosition)
-        # window.draw(logoSurface, logoPosition)
         
     def drawMenu(self, surface):
         """ Draws the Menu to the window """
         menuSurface = self.menuView.draw()
-        menuPosition = self.getCenteredRect(surface, menuSurface, .5, 11.0/16)
+        menuPosition = self.getCenteredPostition(menuSurface, .5, 11.0/16)
         surface.blit(menuSurface, menuPosition)
-        # window.draw(menuSurface, menuPosition)
         
