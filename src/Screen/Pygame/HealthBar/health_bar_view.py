525d156df40f811b3c07d946bc6592e987357066
Chris Loew <cloew123@gmail.com>
2014-02-17 14:36:09 -0700

    Health Bar now inherits from KaoGUI Pygame Widget

--- src/Screen/Pygame/HealthBar/health_bar_view.py
+++ src/Screen/Pygame/HealthBar/health_bar_view.py
@@ -1,6 +1,7 @@
-from Screen.Pygame.pygame_helper import GetTransparentSurface
+from kao_gui.pygame.pygame_helper import GetTransparentSurface
+from kao_gui.pygame.pygame_widget import PygameWidget
 
-class HealthBarView:
+class HealthBarView(PygameWidget):
     """ View for a Pokemon's Health Bar """
     
     def __init__(self, pokemon):
@@ -9,19 +10,18 @@ class HealthBarView:
         
     def setSize(self, width, height):
         """ Set the size of the widget """
-        self.height = height
-        self.width = width
+        self.__height = height
+        self.__width = width
         
-    def draw(self):
+    def buildSurface(self):
+        """ Return the surface for the widget """
+        return GetTransparentSurface(self.__width, self.__height)
+        
+    def drawSurface(self):
         """ Draw the Health Bar View and return its surface """
-        surface = GetTransparentSurface(self.width, self.height)
         healthBarSurface = GetTransparentSurface(self.width*self.getHealthPercentage()/100, self.height)
         healthBarSurface.fill(self.getHealthBarColor())
-        surface.blit(healthBarSurface, (0,0))
-        return surface
-        
-    def update(self):
-        """ Do nothing """
+        self.drawOnSurface(healthBarSurface, left=0, top=0)
         
     def getHealthBarColor(self):
         """ Returns the Health Bar's color """
