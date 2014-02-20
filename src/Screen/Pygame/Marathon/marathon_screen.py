85801f47f3c0847fa31a357e60fed61a411dc36c
Chris Loew <cloew123@gmail.com>
2014-02-19 18:57:14 -0700

    Description of the Marathon is now shown on the splashscreen

--- src/Screen/Pygame/Marathon/marathon_screen.py
+++ src/Screen/Pygame/Marathon/marathon_screen.py
@@ -2,6 +2,7 @@ from resources.resource_manager import GetImagePath
 
 from kao_gui.pygame.pygame_helper import load_image
 from kao_gui.pygame.pygame_screen import PygameScreen
+from kao_gui.pygame.widgets.label import Label
 
 class MarathonScreen(PygameScreen):
     """ Screen for a Pokemon Marathon """
@@ -11,10 +12,13 @@ class MarathonScreen(PygameScreen):
         PygameScreen.__init__(self)
         self.marathon = marathon
         self.backgroundImage = load_image(GetImagePath("Marathons/Kanto_Gym_Leaders.jpg"))
+        self.descriptionLabel = Label(marathon.description)
         
     def drawSurface(self):
         """ Draw the screen """
         self.drawOnSurface(self.backgroundImage, left=0, top=0)
+        labelSurface = self.descriptionLabel.draw()
+        self.drawOnSurface(labelSurface, left=.05, centery=.1)
                     
     def update(self):
         """ Do Nothing """
