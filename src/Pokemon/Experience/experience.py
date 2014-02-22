a621795f5c4ada14a6fd4832d3c300ba70049ead
Chris Loew <cloew123@gmail.com>
2014-02-22 00:54:02 -0700

    Added the Medium Fast Experience Formula

---
+++ src/Pokemon/Experience/experience.py
@@ -0,0 +1,7 @@
+
+class Experience:
+    """ Represents an Experince Formula """
+    
+    def getExperinceForLevel(self, level):
+        """ Return the experience needed for the given level """
+        return NotImplemented
---
+++ src/Pokemon/Experience/medium_fast_experience.py
@@ -0,0 +1,7 @@
+
+class MediumFastExperience:
+    """ Represents the Medium Fast Experience Formula """
+    
+    def getExperinceForLevel(self, level):
+        """ Return the experience needed for the given level """
+        return level**3
