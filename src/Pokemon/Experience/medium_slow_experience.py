48005b6a6eac871539ff5915a6c62d71ed7e94a9
Chris Loew <cloew123@gmail.com>
2014-02-22 00:59:36 -0700

    Added Medium Slow Experience formula

---
+++ src/Pokemon/Experience/medium_slow_experience.py
@@ -0,0 +1,7 @@
+
+class MediumSlowExperience:
+    """ Represents the Medium Slow Experience Formula """
+    
+    def getExperinceForLevel(self, level):
+        """ Return the experience needed for the given level """
+        return int(6.0*(level**3)/5 + 15*(level**2) + 100*level - 140)
