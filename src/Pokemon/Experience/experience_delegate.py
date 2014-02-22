bb265707332f08d0ed55b206d92db22a83c23a3a
Chris Loew <cloew123@gmail.com>
2014-02-22 10:41:45 -0700

    Added Experience Delegate

---
+++ src/Pokemon/Experience/experience_delegate.py
@@ -0,0 +1,20 @@
+
+class ExperienceDelegate:
+    """ Represents the Pokemon's Experience Delegate """
+    
+    def __init__(self, currentExperince, baseExperience, experienceRate, parent):
+        """ Initialize the Experience Delegate """
+        self.currentExperience = currentExperience
+        self.baseExperience = baseExperience
+        self.experieceRate = experienceRate
+        self.parent = parent
+        
+    @property
+    def experinceToNextLevel(self):
+        """ Return the experince to the next level """
+        return self.experinceForNextLevel - self.currentExperience
+        
+    @property
+    def experinceForNextLevel(self):
+        """ Return the experince for the next level """
+        return self.experienceRate.getExperinceForLevel(self.parent.level+1)
