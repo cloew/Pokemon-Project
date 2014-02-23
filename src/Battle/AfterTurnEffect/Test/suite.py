7cfcec7b5415190c3846981311dd8de51c097fee
Chris Loew <cloew123@gmail.com>
2014-02-22 19:32:13 -0700

    Tidied up the After Turn Effect Interface

--- src/Battle/AfterTurnEffect/Test/suite.py
+++ src/Battle/AfterTurnEffect/Test/suite.py
@@ -1,6 +1,6 @@
 import unittest
 
-import after_turn_test
+from Battle.AfterTurnEffect.Test.after_turn_effect_test import suite as after_turn_effect_suite
 
-suites = [after_turn_test.suite]
+suites = [after_turn_effect_suite]
 suite = unittest.TestSuite(suites)
