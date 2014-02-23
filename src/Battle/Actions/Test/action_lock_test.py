42c9fddac5f20aaef5756d5ffded0d6d351aa148
Chris Loew <cloew123@gmail.com>
2014-02-22 18:48:57 -0700

    Cleaned up action lcok useAction Tests

--- src/Battle/Actions/Test/action_lock_test.py
+++ src/Battle/Actions/Test/action_lock_test.py
@@ -12,18 +12,37 @@ class useAction(unittest.TestCase):
         attack = AttackFactory.getAttackAsNew("TACKLE")
         self.action = AttackAction(attack, None, None, None)
         self.turns = 2
-        self.actionLock = ActionLock(None, self.action, self.turns)
+        self.actionLock = ActionLock(self, self.action, self.turns)
         
-    def useAction_correctly(self):
-        """ Checks if useAction changes variables appropriately """
+    def actionReturned(self):
+        """ Checks if useAction returns the proper Action """
         action = self.actionLock.useAction()
         
         assert action is self.action, "Action should be the action the lock was built with"
+        
+    def turnsDecreased(self):
+        """ Checks that the turns counter is decreased """
+        action = self.actionLock.useAction()
+        
         assert self.actionLock.turnsToGo == self.turns-1, \
             "turnsToGo in the lock should be decresed by 1"
+            
+    def parentLockRemoved_unfinished(self):
+        """ Checks if useAction changes variables appropriately """
+        self.actionLock.turnsToGo = 2
+        action = self.actionLock.useAction()
+        
+        assert self.actionLock is not None, "Parent action lock should remain when not finished"
+            
+    def parentLockRemoved_finished(self):
+        """ Checks if useAction changes variables appropriately """
+        self.actionLock.turnsToGo = 1
+        action = self.actionLock.useAction()
+        
+        assert self.actionLock is None, "Parent action lock should be removed when finished"
         
 # Collect all test cases in this class
-testcasesUseAction = ["useAction_correctly"]
+testcasesUseAction = ["actionReturned", "turnsDecreased", "parentLockRemoved_unfinished", "parentLockRemoved_finished"]
 suiteUseAction = unittest.TestSuite(map(useAction, testcasesUseAction))
 
 ##########################################################
