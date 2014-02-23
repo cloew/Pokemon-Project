03ec691244e1e7719fccd9c9c3d381624f52a74c
Chris Loew <cloew123@gmail.com>
2014-02-23 13:31:50 -0700

    Attack now uses Steps when used

--- src/Battle/Attack/Test/attack_test.py
+++ src/Battle/Attack/Test/attack_test.py
@@ -2,6 +2,15 @@ from Battle.Attack.attack import Attack
 
 import unittest
 
+class MockStep:
+    def __init__(self, messages):
+        self.messages = messages
+        self.performed = False
+        
+    def perform(self, user, target, environment):
+        self.performed = True
+        return self.messages
+
 class use(unittest.TestCase):
     """ Test cases of use """
     
@@ -9,293 +18,63 @@ class use(unittest.TestCase):
         """ Build the Attack for the test """
         self.attack = Attack()
         self.messages = ["My Test Messages"]
-        self.preconditionMessage = "My Precondition Message"
         
-        self.calledPreconditions = False
-        self.calledDoAttack = False
+        self.attack.preconditionsStep = MockStep(self.messages)
+        self.attack.hitStep = MockStep(self.messages)
+        self.attack.damageStep = MockStep(self.messages)
+        self.attack.effectsStep = MockStep(self.messages)
+        self.attack.handleContactStep = MockStep(self.messages)
+        self.attack.handleMissEffectsStep = MockStep(self.messages)
         
     def preconditions_Failing(self):
         """ Test that failing precodnitions prevents the attack from being done """
-        self.attack.checkPreconditions = self.failPreconditions
-        self.attack.doAttack = self.doAttack
+        self.attack.preconditionsStep.passed = False
         
         self.attack.use(None, None, None)
         
-        self.assertTrue(self.calledPreconditions, "Should have called checkPreconditions")
-        self.assertFalse(self.calledDoAttack, "Should not have called doAttack")
+        self.assertTrue(self.attack.preconditionsStep.performed, "Should have performed the Precondition Step")
+        self.assertFalse(self.attack.hitStep.performed, "Should not have performed the Hit Step")
+        self.assertFalse(self.attack.damageStep.performed, "Should not have performed the Damage Step")
+        self.assertFalse(self.attack.effectsStep.performed, "Should not have performed the Effects Step")
+        self.assertFalse(self.attack.handleContactStep.performed, "Should not have performed the Handle Contact Step")
+        self.assertFalse(self.attack.handleMissEffectsStep.performed, "Should not have performed the Hanlde Miss Effects Step")
         
-    def preconditions_Passing(self):
+    def hit_Succeeded(self):
         """ Test that passing precodnitions allows the attack to be used """
-        self.attack.checkPreconditions = self.passPreconditions
-        self.attack.doAttack = self.doAttack
+        self.attack.preconditionsStep.passed = True
+        self.attack.hitStep.hit = True
         
         self.attack.use(None, None, None)
         
-        self.assertTrue(self.calledPreconditions, "Should have called checkPreconditions")
-        self.assertTrue(self.calledDoAttack, "Should have called doAttack")
+        self.assertTrue(self.attack.preconditionsStep.performed, "Should have performed the Precondition Step")
+        self.assertTrue(self.attack.hitStep.performed, "Should not have performed the Hit Step")
+        self.assertTrue(self.attack.damageStep.performed, "Should have performed the Damage Step")
+        self.assertTrue(self.attack.effectsStep.performed, "Should have performed the Effects Step")
+        self.assertTrue(self.attack.handleContactStep.performed, "Should have performed the Handle Contact Step")
+        self.assertFalse(self.attack.handleMissEffectsStep.performed, "Should not have performed the Hanlde Miss Effects Step")
     
-    def messagesReturned_Failing(self):
+    def hit_Missed(self):
         """ Test that messages from the attack are returned """
-        self.attack.checkPreconditions = self.failPreconditions
-        self.attack.doAttack = self.doAttack
+        self.attack.preconditionsStep.passed = True
+        self.attack.hitStep.hit = False
         
-        messages = self.attack.use(None, None, None)
+        self.attack.use(None, None, None)
         
-        self.assertIn(self.preconditionMessage, messages, "Should have returned the message from the Preconditions check")
-        self.assertEquals(1, len(messages), "Should only have returned the Preconditions Messages")
-    
-    def messagesReturned_Passing(self):
-        """ Test that messages from the attack are returned """
-        self.attack.checkPreconditions = self.passPreconditions
-        self.attack.doAttack = self.doAttack
-        
-        messages = self.attack.use(None, None, None)
-
-        expectedMessages = self.messages + [self.preconditionMessage]
-        
-        self.assertIn(self.preconditionMessage, messages, "Should have returned the message from the Preconditions check")
-        for message in expectedMessages:
-            self.assertIn(message, messages, "Should have returned each message from the Attack")
-        self.assertEquals(len(expectedMessages), len(messages), "Should only have returned the Precondition and Attack Messages")
-        
-    def failPreconditions(self, preconditionChecker, messages):
-        self.calledPreconditions = True
-        messages.append(self.preconditionMessage)
-        return True
-        
-    def passPreconditions(self, preconditionChecker, messages):
-        self.calledPreconditions = True
-        messages.append(self.preconditionMessage)
-        return False
-        
-    def doAttack(self, user, target, environment):
-        self.calledDoAttack = True
-        return self.messages
+        self.assertTrue(self.attack.preconditionsStep.performed, "Should have performed the Precondition Step")
+        self.assertTrue(self.attack.hitStep.performed, "Should have performed the Hit Step")
+        self.assertFalse(self.attack.damageStep.performed, "Should not have performed the Damage Step")
+        self.assertFalse(self.attack.effectsStep.performed, "Should not have performed the Effects Step")
+        self.assertFalse(self.attack.handleContactStep.performed, "Should not have performed the Handle Contact Step")
+        self.assertTrue(self.attack.handleMissEffectsStep.performed, "Should have performed the Hanlde Miss Effects Step")
 
 # Collect all test cases in this class
-testcasesUse = ["preconditions_Failing", "preconditions_Passing", "messagesReturned_Failing", "messagesReturned_Passing"]
+testcasesUse = ["preconditions_Failing", "hit_Succeeded", "hit_Missed"]
 suiteUse = unittest.TestSuite(map(use, testcasesUse))
 
 ##########################################################
 
-class doAttack(unittest.TestCase):
-    """ Test cases of doAttack """
-    
-    def  setUp(self):
-        """ Build the Attack for the test """
-        self.attack = Attack()
-        self.attack.name = "Test Attack"
-        self.messages = ["My Test Messages"]
-        self.hitMessage = "My Precondition Message"
-        self.attackUsedMessage = "%s USED %s" % (self.getHeader(), self.attack.name)
-        
-        self.calledDoHit = False
-        self.calledDoAttackLoop = False
-        
-    def hit_Failing(self):
-        """ Test that failing hit prevents the attack loop from being done """
-        self.attack.doHit = self.failHit
-        self.attack.doAttackLoop = self.doAttackLoop
-        
-        self.attack.doAttack(self, None, None)
-        
-        self.assertTrue(self.calledDoHit, "Should have called doHit")
-        self.assertFalse(self.calledDoAttackLoop, "Should not have called doAttackLoop")
-        
-    def hit_Passing(self):
-        """ Test that passing hit allows the attack loop to be done """
-        self.attack.doHit = self.passHit
-        self.attack.doAttackLoop = self.doAttackLoop
-        
-        self.attack.doAttack(self, None, None)
-        
-        self.assertTrue(self.calledDoHit, "Should have called doHit")
-        self.assertTrue(self.calledDoAttackLoop, "Should have called doAttackLoop")
-    
-    def messagesReturned_Failing(self):
-        """ Test that messages from the hit attempt are returned """
-        self.attack.doHit = self.failHit
-        self.attack.doAttackLoop = self.doAttackLoop
-        
-        messages = self.attack.doAttack(self, None, None)
-        
-        self.assertEquals(self.attackUsedMessage, messages[0], "Should have returned the attack used message")
-        self.assertEquals(self.hitMessage, messages[1], "Should have returned the message from the Hit check")
-        self.assertEquals(2, len(messages), "Should only have returned the Attack Used & Hit Messages")
-    
-    def messagesReturned_Passing(self):
-        """ Test that messages from the attack and hit attempt are returned """
-        self.attack.doHit = self.passHit
-        self.attack.doAttackLoop = self.doAttackLoop
-        
-        messages = self.attack.doAttack(self, None, None)
-
-        expectedMessages = [self.attackUsedMessage, self.hitMessage] + self.messages
-        
-        for expectedMessage in expectedMessages:
-            i = expectedMessages.index(expectedMessage)
-            message = messages[i]
-            self.assertEquals(expectedMessage, message, "Should have returned each message in the proper order")
-        self.assertEquals(len(expectedMessages), len(messages), "Should only have returned the Attack Used, Hit and Attack Messages")
-        
-    def failHit(self, user, target, environment, messages):
-        self.calledDoHit = True
-        messages.append(self.hitMessage)
-        return False
-        
-    def passHit(self, user, target, environment, messages):
-        self.calledDoHit = True
-        messages.append(self.hitMessage)
-        return True
-        
-    def doAttackLoop(self, user, target, environment):
-        self.calledDoAttackLoop = True
-        return self.messages
-        
-    def getHeader(self):
-        return "My Header"
-
-# Collect all test cases in this class
-testcasesDoAttack = ["hit_Failing", "hit_Passing", "messagesReturned_Failing", "messagesReturned_Passing"]
-suiteDoAttack = unittest.TestSuite(map(doAttack, testcasesDoAttack))
-
-##########################################################
-
-class doAttackLoop(unittest.TestCase):
-    """ Test cases of doAttackLoop """
-    
-    def  setUp(self):
-        """ Build the Attack for the test """
-        self.attack = Attack()
-        self.damageMessages = ["My Damage Messages"]
-        self.effectMessages = ["My Effect Messages"]
-        self.contactMessages = ["My Contact Messages"]
-        
-        self.calledDoDamage = False
-        self.calledDoEffects = False
-        self.calledOnContact = False
-        
-    def makesContact(self):
-        """ Test that failing hit prevents the attack loop from being done """
-        self.attack.makes_contact = True
-        self.attack.doDamage = self.doDamage
-        self.attack.doEffects = self.doEffects
-        
-        self.attack.doAttackLoop(None, self, None)
-        
-        self.assertTrue(self.calledDoDamage, "Should have called doDamage")
-        self.assertTrue(self.calledDoEffects, "Should have called doEffects")
-        self.assertTrue(self.calledOnContact, "Should have called onContact")
-        
-    def noContact(self):
-        """ Test that passing hit allows the attack loop to be done """
-        self.attack.makes_contact = False
-        self.attack.doDamage = self.doDamage
-        self.attack.doEffects = self.doEffects
-        
-        self.attack.doAttackLoop(None, self, None)
-        
-        self.assertTrue(self.calledDoDamage, "Should have called doDamage")
-        self.assertTrue(self.calledDoEffects, "Should have called doEffects")
-        self.assertFalse(self.calledOnContact, "Should not have called onContact")
-    
-    def messagesReturned_NoContact(self):
-        """ Test that messages when no contact is made are returned """
-        self.attack.makes_contact = False
-        self.attack.doDamage = self.doDamage
-        self.attack.doEffects = self.doEffects
-        
-        messages = self.attack.doAttackLoop(None, self, None)
-        
-        expectedMessages = self.damageMessages + self.effectMessages
-        
-        for expectedMessage in expectedMessages:
-            i = expectedMessages.index(expectedMessage)
-            message = messages[i]
-            self.assertEquals(expectedMessage, message, "Should have returned each message in the proper order")
-        self.assertEquals(len(expectedMessages), len(messages), "Should only have returned the Damage and Effect Messages")
-    
-    def messagesReturned_OnContact(self):
-        """ Test that messages when contact is made are returned """
-        self.attack.makes_contact = True
-        self.attack.doDamage = self.doDamage
-        self.attack.doEffects = self.doEffects
-        
-        messages = self.attack.doAttackLoop(None, self, None)
-
-        expectedMessages = self.damageMessages + self.effectMessages + self.contactMessages
-        
-        for expectedMessage in expectedMessages:
-            i = expectedMessages.index(expectedMessage)
-            message = messages[i]
-            self.assertEquals(expectedMessage, message, "Should have returned each message in the proper order")
-        self.assertEquals(len(expectedMessages), len(messages), "Should only have returned the Damage, Effect and Contact Messages")
-        
-    def doDamage(self, user, target, environment):
-        self.calledDoDamage = True
-        return self.damageMessages
-        
-    def doEffects(self, user, target, environment):
-        self.calledDoEffects = True
-        return self.effectMessages
-        
-    def getAbility(self):
-        return self
-        
-    def onContact(self, target, user):
-        self.calledOnContact = True
-        return self.contactMessages
-
-# Collect all test cases in this class
-testcasesDoAttackLoop = ["makesContact", "makesContact", "messagesReturned_NoContact", "messagesReturned_OnContact"]
-suiteDoAttackLoop = unittest.TestSuite(map(doAttackLoop, testcasesDoAttackLoop))
-
-##########################################################
-
-class checkPreconditions(unittest.TestCase):
-    """ Test cases of checkPreconditions """
-    
-    def setUp(self):
-        """ Build the Attack for the test """
-        self.attack = Attack()
-        self.messages = ["My Test Messages"]
-        
-        self.calledCheckPreConditions = False
-        
-    def preconditionsPassthrough(self):
-        """ Test that preconditions stop value is returned """
-        for stopCondition in [True, False]:
-            self.failPreconditions = stopCondition
-            stop = self.attack.checkPreconditions(self, [])
-        
-            self.assertEquals(self.failPreconditions, stop, "Should have returned the failPreconditions parameter")
-            self.assertTrue(self.calledCheckPreConditions, "Should have called checkPrecondtitions")
-    
-    def messagesReturned(self):
-        """ Test that messages when no contact is made are returned """
-        self.failPreconditions = False
-        messages = []
-        
-        self.attack.checkPreconditions(self, messages)
-        
-        for expectedMessage in self.messages:
-            i = self.messages.index(expectedMessage)
-            message = messages[i]
-            self.assertEquals(expectedMessage, message, "Should have returned each message in the proper order")
-        self.assertEquals(len(self.messages), len(messages), "Should only have returned the Precondtition Messages")
-        
-    def checkPreConditions(self):
-        self.calledCheckPreConditions = True
-        return self.failPreconditions, self.messages
-
-# Collect all test cases in this class
-testcasesCheckPreconditions = ["preconditionsPassthrough", "messagesReturned"]
-suiteCheckPreconditions = unittest.TestSuite(map(checkPreconditions, testcasesCheckPreconditions))
-
-##########################################################
-
 # Collect all test cases in this file
-suites = [suiteUse, suiteDoAttack, suiteDoAttackLoop, suiteCheckPreconditions]
+suites = [suiteUse]
 suite = unittest.TestSuite(suites)
 
 if __name__ == "__main__":
--- src/Battle/Attack/attack.py
+++ src/Battle/Attack/attack.py
@@ -4,6 +4,8 @@ from Battle.Attack.DamageDelegates.null_damage_delegate import NullDamageDelegat
 from Battle.Attack.Steps.announcement_step import AnnouncementStep
 from Battle.Attack.Steps.damage_step import DamageStep
 from Battle.Attack.Steps.effects_step import EffectsStep
+from Battle.Attack.Steps.handle_contact_step import HandleContactStep
+from Battle.Attack.Steps.handle_miss_effects_step import HandleMissEffectsStep
 from Battle.Attack.Steps.hit_step import HitStep
 from Battle.Attack.Steps.precondition_step import PreconditionStep
 
@@ -18,77 +20,28 @@ class Attack:
         self.speedDelegate = None
         
         self.effectDelegates = []
-        makes_contact = False
+        self.makes_contact = False
+        
+        self.preconditionsStep = PreconditionStep(self)
+        self.hitStep = HitStep(self)
+        self.damageStep = DamageStep(self)
+        self.effectsStep = EffectsStep(self)
+        self.handleContactStep = HandleContactStep(self)
+        self.handleMissEffectsStep = HandleMissEffectsStep(self)
         
     def use(self, user, target, environment):
         """ Uses the current attack Object in a Battle """
         messages = []
-        # preconditionChecker = PreconditionChecker(user, target, environment, self)
-        # stop = self.checkPreconditions(preconditionChecker, messages)
+        messages += self.preconditionsStep.perform(user, target, environment)
+        if self.preconditionsStep.passed:
+            messages += self.hitStep.perform(user, target, environment)
+            if self.hitStep.hit:
+                messages += self.damageStep.perform(user, target, environment)
+                messages += self.effectsStep.perform(user, target, environment)
+                messages += self.handleContactStep.perform(user, target, environment)
+            else:
+                messages += self.handleMissEffectsStep.perform(user, target, environment)
         
-        preconditionStep = PreconditionStep(self)
-        messages += preconditionStep.perform(user, target, environment)
-        
-        # if not stop:
-        if preconditionStep.passed:
-            messages += self.doAttack(user, target, environment)
-        
-        return messages
-        
-    def doAttack(self, user, target, environment):
-        """  """
-        # messages = ["%s USED %s" % (user.getHeader(), self.name)]
-        announcementStep = AnnouncementStep(self)
-        messages = announcementStep.perform(user, target, environment)
-        # hit = self.doHit(user, target, environment, messages)
-        hitStep = HitStep(self)
-        messages += hitStep.perform(user, target, environment)
-        
-        # if hit:
-        if hitStep.hit:
-            messages += self.doAttackLoop(user, target, environment)
-        
-        return messages
-        
-    def doAttackLoop(self, user, target, environment):
-        """  """
-        messages = []
-        # messages += self.doDamage(user, target, environment)
-        damageStep = DamageStep(self)
-        messages += damageStep.perform(user, target, environment)
-        # messages += self.doEffects(user, target, environment)
-        effectsStep = EffectsStep(self)
-        messages += effectsStep.perform(user, target, environment)
-        if self.makes_contact:
-            messages += target.getAbility().onContact(target, user)
-        return messages
-        
-    # Attack Loop Functions
-    def checkPreconditions(self, preconditionChecker, messages):
-        """ Checks preconditions to make sure the user isn't prevented from working """
-        stop, preMessages = preconditionChecker.checkPreConditions()
-        messages += preMessages
-        return stop
-        
-    def doHit(self, user, target, environment, messages):
-        """ Check if the user hits the target(s) """
-        hit, hitMessages = self.hitDelegate.hit(user, target, environment)
-        if not hit:
-            messages += hitMessages
-            messages += self.applyEffectsOnMiss(user, target, environment)
-        return hit
-        
-    def doDamage(self, user, target, environment):
-        """ Does the attack's damage, returns that the loop should not stop """
-        return self.damageDelegate.doDamage(user, target, environment)
-        
-    def doEffects(self, user, target, environment):
-        """ Does the attack's effects, returns that the loop should not stop """
-        messages = []
-
-        if not self.preventEffects(user, target):
-            for effect in self.effectDelegates:
-                messages += effect.tryToApplyEffect(user, target, environment)
         return messages
         
     # Helper Methods
@@ -98,24 +51,6 @@ class Attack:
             self.effectDelegates.append(delegate)
             return
         setattr(self, delegateCategory, delegate)
-        
-    def preventEffects(self, user, target):
-        """ Return whether the effects are prevented """
-        nullDamage = hasattr(self.damageDelegate, "isNull")
-        canUse = user.getAbility().canUseEffects() and target.getAbility().canUseEffects()
-        return not nullDamage and not canUse
-        
-    def applyEffectsOnMiss(self, user, target, environment):
-        """ Apply effects on miss """
-        messages = []
-        for effect in self.effectDelegates:
-            if hasattr(effect, "applyOnMiss"):
-                effectMessages = effect.applyEffect(user, target, environment)
-                messages = messages + effectMessages
-            if hasattr(effect, "effectOnMiss"):
-                effectMessages = effect.effectOnMiss(user, target, environment)
-                messages = messages + effectMessages
-        return messages
         
     def isStatus(self):
         """ Returns if the Attack is a status attack """
