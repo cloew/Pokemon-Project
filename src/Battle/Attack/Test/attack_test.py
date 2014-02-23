c02bcb3488bdaef8a0f3aa2d9076e7420cb86d33
Chris Loew <cloew123@gmail.com>
2014-02-22 20:13:42 -0700

    Added test of the Attack do Attack function

--- src/Battle/Attack/Test/attack_test.py
+++ src/Battle/Attack/Test/attack_test.py
@@ -78,8 +78,91 @@ suiteUse = unittest.TestSuite(map(use, testcasesUse))
 
 ##########################################################
 
+class doAttack(unittest.TestCase):
+    """ Test cases of doAttack """
+    
+    def  setUp(self):
+        """ Build the Attack for the test """
+        self.attack = Attack()
+        self.attack.name = "Test Attack"
+        self.messages = ["My Test Messages"]
+        self.hitMessage = "My Precondition Message"
+        self.attackUsedMessage = "%s USED %s" % (self.getHeader(), self.attack.name)
+        
+        self.calledDoHit = False
+        self.calledDoAttackLoop = False
+        
+    def hit_Failing(self):
+        """ Test that failing hit prevents the attack loop from being done """
+        self.attack.doHit = self.failHit
+        self.attack.doAttackLoop = self.doAttackLoop
+        
+        self.attack.doAttack(self, None, None)
+        
+        self.assertTrue(self.calledDoHit, "Should have called doHit")
+        self.assertFalse(self.calledDoAttackLoop, "Should not have called doAttackLoop")
+        
+    def hit_Passing(self):
+        """ Test that passing hit allows the attack loop to be done """
+        self.attack.doHit = self.passHit
+        self.attack.doAttackLoop = self.doAttackLoop
+        
+        self.attack.doAttack(self, None, None)
+        
+        self.assertTrue(self.calledDoHit, "Should have called doHit")
+        self.assertTrue(self.calledDoAttackLoop, "Should have called doAttackLoop")
+    
+    def messagesReturned_Failing(self):
+        """ Test that messages from the hit attempt are returned """
+        self.attack.doHit = self.failHit
+        self.attack.doAttackLoop = self.doAttackLoop
+        
+        messages = self.attack.doAttack(self, None, None)
+        
+        self.assertEquals(self.attackUsedMessage, messages[0], "Should have returned the attack used message")
+        self.assertEquals(self.hitMessage, messages[1], "Should have returned the message from the Hit check")
+        self.assertEquals(2, len(messages), "Should only have returned the Attack Used & Hit Messages")
+    
+    def messagesReturned_Passing(self):
+        """ Test that messages from the attack and hit attempt are returned """
+        self.attack.doHit = self.passHit
+        self.attack.doAttackLoop = self.doAttackLoop
+        
+        messages = self.attack.doAttack(self, None, None)
+
+        expectedMessages = [self.attackUsedMessage, self.hitMessage] + self.messages
+        
+        for expectedMessage in expectedMessages:
+            i = expectedMessages.index(expectedMessage)
+            message = messages[i]
+            self.assertEquals(expectedMessage, message, "Should have returned each message in the proper order")
+        self.assertEquals(len(expectedMessages), len(messages), "Should only have returned the Attack Used, Hit and Attack Messages")
+        
+    def failHit(self, user, target, environment, messages):
+        self.calledDoHit = True
+        messages.append(self.hitMessage)
+        return False
+        
+    def passHit(self, user, target, environment, messages):
+        self.calledDoHit = True
+        messages.append(self.hitMessage)
+        return True
+        
+    def doAttackLoop(self, user, target, environment):
+        self.calledDoAttackLoop = True
+        return self.messages
+        
+    def getHeader(self):
+        return "My Header"
+
+# Collect all test cases in this class
+testcasesDoAttack = ["hit_Failing", "hit_Passing", "messagesReturned_Failing", "messagesReturned_Passing"]
+suiteDoAttack = unittest.TestSuite(map(doAttack, testcasesDoAttack))
+
+##########################################################
+
 # Collect all test cases in this file
-suites = [suiteUse]
+suites = [suiteUse, suiteDoAttack]
 suite = unittest.TestSuite(suites)
 
 if __name__ == "__main__":
