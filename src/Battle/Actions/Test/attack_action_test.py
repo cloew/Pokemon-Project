84f3e6129f380f3e36e6dd03fe87aee5512298c4
Chris Loew <cloew123@gmail.com>
2014-02-22 18:56:38 -0700

    Added test to ensure doing an attack action usese the attack

--- src/Battle/Actions/Test/attack_action_test.py
+++ src/Battle/Actions/Test/attack_action_test.py
@@ -3,6 +3,11 @@ import unittest
 from Battle.Actions.attack_action import AttackAction
 from Battle.Attack.attackfactory import AttackFactory
 
+class AttakMock:
+    def use(self, user, target, environment):
+        self.used = True
+
+
 class getPriority(unittest.TestCase):
     """ Test cases of getPriority """
     
@@ -21,8 +26,33 @@ testcasesGetPriority = []
 suiteGetPriority = unittest.TestSuite(map(getPriority, testcasesGetPriority))
 
 ##########################################################
+
+class doAction(unittest.TestCase):
+    """ Test cases of doAction """
+    
+    def  setUp(self):
+        """ Build the Attack Action """
+        attack = AttackFactory.getAttackAsNew("TACKLE")
+        self.priority = attack.speedDelegate.priority
+        attack.use = self.use
+        self.action = AttackAction(attack)
+        self.usedAttack = False
+        
+    def attackUsed(self):
+        """ Test that the priority is returned correctly """
+        assert self.usedAttack, "Should have used the Action's Attack"
+        
+    def use(self, user, target, environment):
+        self.usedAttack = True
+
+# Collect all test cases in this class
+testcasesDoAction = []
+suiteDoAction = unittest.TestSuite(map(doAction, testcasesDoAction))
+
+##########################################################
+
 # Collect all test cases in this file
-suites = [suiteGetPriority]
+suites = [suiteGetPriority, suiteDoAction]
 suite = unittest.TestSuite(suites)
 
 if __name__ == "__main__":
--- src/Battle/Test/suite.py
+++ src/Battle/Test/suite.py
@@ -5,11 +5,13 @@ import battle_message_test
 import battleside_test
 import pkmn_battle_wrapper_test
 
-import Battle.Weather.Test.suite as weather_suite
+from Battle.Actions.Test.suite import suite as action_suite
+from Battle.Weather.Test.suite import suite as weather_suite
 
 suites = [battle_environment_test.suite,
           battle_message_test.suite,
           battleside_test.suite,
           pkmn_battle_wrapper_test.suite,
-          weather_suite.suite]
+          action_suite,
+          weather_suite]
 suite = unittest.TestSuite(suites)
