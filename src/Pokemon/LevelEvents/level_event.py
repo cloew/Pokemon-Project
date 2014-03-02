58e1df737c9fe6d846bdcac85bccc716cb3f2117
Chris Loew <cloew123@gmail.com>
2014-03-02 11:41:06 -0700

    Added a Level Event class

---
+++ src/Pokemon/LevelEvents/level_event.py
@@ -0,0 +1,7 @@
+
+class LevelEvent:
+    """ Represents an event that should occur after leveling up """
+    
+    def __init__(self, pokemon):
+        """ Initialize the Level Event """
+        self.pokemon = pokemon
