67e03e1635f137f58ca51e0861ff483af9601608
Chris Loew <cloew123@gmail.com>
2014-03-08 09:56:35 -0700

    Added the Pressure Ability

---
+++ src/Pokemon/Abilities/pressure_ability.py
@@ -0,0 +1,7 @@
+
+class PressureAbility:
+    """ Represents the Pressure Ability """
+    
+    def powerPointsPressure(self):
+        """ Return the power point Pressure this Pokemon applies """
+        return 2
