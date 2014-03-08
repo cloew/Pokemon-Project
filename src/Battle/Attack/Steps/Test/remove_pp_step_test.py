83692d420aad26e9a222551528933c1812bca289
Chris Loew <cloew123@gmail.com>
2014-03-08 09:53:02 -0700

    Remove PP now uses the Targets pressure if attacking a target

--- src/Battle/Attack/Steps/Test/remove_pp_step_test.py
+++ src/Battle/Attack/Steps/Test/remove_pp_step_test.py
@@ -1,6 +1,8 @@
 from Battle.Attack.attack import Attack
+from Battle.Attack.HitDelegates.hitself_delegate import HitSelfDelegate
 from Battle.Attack.Steps.remove_pp_step import RemovePPStep
 
+from Test.test_helper import BuildPokemonBattleWrapper
 import unittest
 
 class perform(unittest.TestCase):
@@ -12,14 +14,31 @@ class perform(unittest.TestCase):
         self.originalPowerPointsValue = 10
         self.attack.currPowerPoints = self.originalPowerPointsValue
         self.step = RemovePPStep(self.attack)
+        self.target = BuildPokemonBattleWrapper()
+        self.target.getAbility().powerPointsPressure = self.powerPointsPressure
+        
+        self.pressure = 2
+        self.usedAbility = False
         
     def standard(self):
         """ Test that the PP is decreased under normal circumstances """
         ppValue = 10
         self.attack.currPowerPoints = ppValue
         
-        self.step.perform(None, None, None)
+        self.step.perform(None, self.target, None)
         
+        self.assertTrue(self.usedAbility, "PP Pressure should have been determined by the Target's Pressure")
+        self.assertEquals(ppValue-self.pressure, self.attack.currPowerPoints, "PP should have been reduced by the Target's Pressure")
+        
+    def hitSelf(self):
+        """ Test that the PP is decreased under normal circumstances """
+        self.attack.hitDelegate = HitSelfDelegate()
+        ppValue = 10
+        self.attack.currPowerPoints = ppValue
+        
+        self.step.perform(None, self.target, None)
+        
+        self.assertFalse(self.usedAbility, "PP Pressure should not have been determined by the Target's Pressure")
         self.assertEquals(ppValue-1, self.attack.currPowerPoints, 'PP should have been reduced by 1')
         
     def zero(self):
@@ -27,12 +46,17 @@ class perform(unittest.TestCase):
         ppValue = 0
         self.attack.currPowerPoints = ppValue
         
-        self.step.perform(None, None, None)
+        self.step.perform(None, self.target, None)
         
         self.assertEquals(0, self.attack.currPowerPoints, 'PP should stay at 0')
+        
+    def powerPointsPressure(self):
+        """ Return the power Point Pressure """
+        self.usedAbility = True
+        return self.pressure
 
 # Collect all test cases in this class
-testcasesPerform = ["standard", "zero"]
+testcasesPerform = ["standard", "hitSelf", "zero"]
 suitePerform = unittest.TestSuite(map(perform, testcasesPerform))
 
 ##########################################################
--- src/Battle/Attack/Steps/remove_pp_step.py
+++ src/Battle/Attack/Steps/remove_pp_step.py
@@ -1,10 +1,20 @@
 from Battle.Attack.Steps.attack_step import AttackStep
+from Battle.Attack.HitDelegates.hitself_delegate import HitSelfDelegate
 
 class RemovePPStep(AttackStep):
     """ Represents the Remove PP Step in the Attack Process """
     
     def perform(self, user, target, environment):
         """ Perform this step """
+        pressure = self.getPressure(target)
+        
         if self.parent.currPowerPoints > 0:
-            self.parent.currPowerPoints -= 1
+            self.parent.currPowerPoints -= pressure
         return []
+        
+    def getPressure(self, target):
+        """ Return the Pressure exerted when using the attack """
+        if isinstance(self.parent.hitDelegate, HitSelfDelegate):
+            return 1
+        else:
+            return target.getAbility().powerPointsPressure()
